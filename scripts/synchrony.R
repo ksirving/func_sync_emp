757ea5333fb0ad3db246785b2067b119f7ce00ee
ksirving <32795054+ksirving@users.noreply.github.com>
2020-08-10 09:18:36 -0700

    leave one out synchrony calculaterd between sites

--- scripts/synchrony.R
+++ scripts/synchrony.R
@@ -55,114 +55,199 @@ pca_basins
 ### loop over basins
 basinsID<-unique(pca_basins$MAIN_BAS) # 46 basins
 Nbasins<-length(unique(pca_basins$MAIN_BAS))
-basinsID
 
-synchrony_a1 = synchrony_a2 = NULL
+nlevels(factor(pca_basins$MAIN_BAS)) 
+synchrony_axis = NULL
 
-basin = 1
 
 for (basin in 1:length(basinsID)) {
   basindata<-pca_basins[pca_basins$MAIN_BAS==basinsID[basin],]
-  head(basindata)
+
   #//////////////////////////////////////////////////////
   #data preparation
   #/////////////////////////////////////////////////////
   
   
-  ## select only axis 1 (fish size and put in columns)
-  datasites <- basindata %>% 
-    select(-c(Axis2,X, site_year, sYNGEO_ID) )%>%
-    spread(site_ID, Axis1) #%>%
+  basindata_melt <- basindata %>% select(-X) %>%
+    reshape2::melt(id= c("site_year", "site_ID", "MAIN_BAS", "origin", "sYNGEO_ID", "year")) %>%
+    rename(axis=variable, score = value)
     
+  ## test the below to see if needed
   
-  sync_data <- ungroup(datasites)
-  sync_data
+  # length(unique(basindata_melt$site_ID))
+  # calculate (temporal) prevalence per site (filter scores present in at least 80% site/years)
+  # timserieslength<-length(unique(basindata$year))
   
-  ## need to remove NAs before correlation
-  cor(select(sync_data, 4:27))
-  
-  
-  
+  # site_consistency<- basindata_melt %>%
+  #   group_by(sYNGEO_ID, axis) %>%
+  #   summarize(prop.occurrence=length(unique(year))/timserieslength) %>%
+  #   subset(prop.occurrence>=.8) 
+  # 
+  # basindata_melt$site_ID %in% site_consistency$sYNGEO_ID ## check all match
+  # 
 
-  
-  
-  
-  ####select species present in at least two sites
-  spp_by_site<-names(which(apply(ifelse(table(basindata$New_names,basindata$sYNGEO_ID) >0,1,0),1,sum)>=2))
-  
-  # now subset the matrix by 'widespread enough' species
-  alldata<-basindata[which(basindata$New_names %in% spp_by_site),]
-  
-  ####select species present in 80% of the years/site combinaison
-  # create abundance matrix per year
-  alldata <- alldata %>%
-    group_by(sYNGEO_ID,New_names,Year) %>%
-    summarize(Abundance=sum(Abundances))
-  
-  # calculate (temporal) prevalence per site (filter species present in at least 80% sampling events)
-  timserieslength<-length(unique(alldata$Year))
-  
-  spp_consistency<- alldata %>%
-    group_by(sYNGEO_ID, New_names) %>%
-    summarize(prop.occurrence=length(unique(Year))/timserieslength) %>%
-    subset(prop.occurrence>=.8) 
-  
-  #create matrix
-  spp_consistency$Speciescombination<-paste0(spp_consistency$sYNGEO_ID,spp_consistency$New_names) # create spp_I x Species label
-  alldata$sYNGEO_IDxSpeciescombination<-paste0(alldata$sYNGEO_ID,alldata$New_names) # Same on the other side
-  alldata<- alldata[alldata$sYNGEO_IDxSpeciescombination %in% spp_consistency$Speciescombination,]
-  alldata<-alldata[,-which(names(alldata) == "sYNGEO_IDxSpeciescombination")]
-  alldata$New_names<-factor(alldata$New_names)
-  
-  # Now check again for spp that after the previous filtering have been left in a single site
-  remspp<-names(which(apply(ifelse(table(alldata$New_names,alldata$sYNGEO_ID) >0,1,0),1,sum)<2))
-  if(length(remspp) > 0){
-    alldata<-alldata[-which(alldata$New_names %in% remspp),]
-  }
-  
   #///////////////////////////////////////////////////////
-  #compute intraspecific synchrony & variability
+  #compute btween site synchrony
   #///////////////////////////////////////////////////////    
   ### loop over species
-  Nspecies<-length(unique(alldata$New_names))
-  for (spp in 1:Nspecies) {
-    spp_data<-alldata[alldata$New_names==unique(alldata$New_names)[spp],]
-    spp_data<-t(spread(spp_data, sYNGEO_ID, Abundance))[-c(1:2),]
-    sitesIDs<-rownames(spp_data)
-    spp_data<-apply(spp_data, 2, as.numeric)
-    rownames(spp_data)<-sitesIDs
+  Naxis<-length(unique(basindata_melt$axis))
+  # ax <- 1
+  
+  
+  for (ax in 1: Naxis) {
+    axis_data<-basindata_melt[basindata_melt$axis==unique(basindata_melt$axis)[ax],]
+    years <- unique(sort(axis_data$year)) ## define years for columns
+  
+    # make df wide
+    axis_data  <- axis_data %>% 
+      select(-c(site_year, sYNGEO_ID) ) %>%
+      spread(site_ID, score) #%>%
+    # flip data
+      axis_data <- t(axis_data)[-c(1:4),]
     
-    ###intraspecific variability
-    cv<-apply(spp_data,1,sd,na.rm=T)/apply(spp_data,1,mean,na.rm=T)
-    sYNGEO_ID = names(cv)
-    variability_spp<-rbind(variability_spp, cbind(basinsID[basin], 
-                                                  as.character(unique(alldata$New_names)[spp]),
-                                                  cv,sYNGEO_ID))
-    ###intraspecific synchrony
-    correlation<-cor(t(spp_data), use = "pairwise.complete.obs")
+    # define rows and column names and convert to numbers
+    sitesIDs<-rownames(axis_data)
+    colnames(axis_data) <- years
+    axis_data<-apply(axis_data, 2, as.numeric)
+    rownames(axis_data)<-sitesIDs
+   
+    # change NAs to zero - can change later if needed
+    axis_data[which(is.na(axis_data))] <- 0
+  
+    ### synchrony
+    correlation<-cor(t(axis_data), use = "pairwise.complete.obs")
     vector_data_correl<- unmatrix(correlation,byrow=F)
     lower_triangle<-lower.tri(correlation)
     vector_data_triangle<-unmatrix(lower_triangle, byrow=F)
     correl_result<-vector_data_correl[vector_data_triangle]
-    sYNGEO_ID1<-sapply(strsplit(names(correl_result),":"),'[',1)
-    sYNGEO_ID2<-sapply(strsplit(names(correl_result),":"),'[',2)
-    synchrony_spp<-rbind(synchrony_spp, cbind(basinsID[basin], 
-                                              as.character(unique(alldata$New_names)[spp]),
-                                              correl_result,sYNGEO_ID1,sYNGEO_ID2))
+    site_ID1<-sapply(strsplit(names(correl_result),":"),'[',1)
+    site_ID2<-sapply(strsplit(names(correl_result),":"),'[',2)
+    correl_result
+    synchrony_axis<-rbind(synchrony_axis, cbind(basinsID[basin], 
+                                              as.character(unique(basindata_melt$axis)[ax]),
+                                              correl_result,site_ID1,site_ID2))
   }
 }
-synchrony_spp<-data.frame(synchrony_spp)
-colnames(synchrony_spp)<-c("basin_ID", "Species", "Correlation","sYNGEO_ID1","sYNGEO_ID2")
-nlevels(synchrony_spp$basin_ID) 
-nlevels(synchrony_spp$Species) 
+warnings()
+synchrony_axis
+dim(synchrony_axis)
+synchrony_axis<-data.frame(synchrony_axis)
+colnames(synchrony_axis)<-c("basin_ID", "Axis", "Correlation","sYNGEO_ID1","sYNGEO_ID2")
+nlevels(factor(synchrony_axis$basin_ID)) # 46
+nlevels(factor(synchrony_axis$Axis)) # 2
 
-variability_spp<-data.frame(variability_spp)
-colnames(variability_spp)<-c("basin_ID", "Species", "CV","sYNGEO_ID")
-nlevels(variability_spp$basin_ID) 
-nlevels(variability_spp$Species) 
 
 ###save results
-write.csv(variability_spp, "results_intraspecific_variability.csv")
-write.csv(synchrony_spp, "results_intraspecific_synchrony.csv")
+write.csv(synchrony_axis, "output_data/results_between_site_synchrony.csv")
+
+## plot synchrony
+
+## synchrony leaving one year out
+
+### loop over basins
+basinsID<-unique(pca_basins$MAIN_BAS) # 46 basins
+Nbasins<-length(unique(pca_basins$MAIN_BAS))
+basinsID[basin]
+nlevels(factor(pca_basins$MAIN_BAS)) 
+synchrony_axis = NULL
 
 
+for (basin in 29:length(basinsID)) {
+  
+  basindata<-pca_basins[pca_basins$MAIN_BAS==basinsID[basin],]
+  
+  #//////////////////////////////////////////////////////
+  #data preparation
+  #/////////////////////////////////////////////////////
+  
+  
+  basindata_melt <- basindata %>% select(-X) %>%
+    reshape2::melt(id= c("site_year", "site_ID", "MAIN_BAS", "origin", "sYNGEO_ID", "year")) %>%
+    rename(axis=variable, score = value)
+  
+  ## test the below to see if needed
+  
+  # length(unique(basindata_melt$site_ID))
+  # calculate (temporal) prevalence per site (filter scores present in at least 80% site/years)
+  # timserieslength<-length(unique(basindata$year))
+  
+  # site_consistency<- basindata_melt %>%
+  #   group_by(sYNGEO_ID, axis) %>%
+  #   summarize(prop.occurrence=length(unique(year))/timserieslength) %>%
+  #   subset(prop.occurrence>=.8) 
+  # 
+  # basindata_melt$site_ID %in% site_consistency$sYNGEO_ID ## check all match
+  # 
+  
+  #///////////////////////////////////////////////////////
+  #compute btween site synchrony
+  #///////////////////////////////////////////////////////    
+  ### loop over species
+  Naxis<-length(unique(basindata_melt$axis))
+  # ax <- 1
+  
+  ## loop over axis
+  for (ax in 1: Naxis) {
+    axis_data<-basindata_melt[basindata_melt$axis==unique(basindata_melt$axis)[ax],]
+    years <- unique(sort(axis_data$year)) ## define years for columns
+    
+    # make df wide
+    axis_data  <- axis_data %>% 
+      select(-c(site_year, sYNGEO_ID) ) %>%
+      spread(site_ID, score) #%>%
+    # flip data
+    axis_data <- t(axis_data)[-c(1:4),]
+    
+    # define rows and column names and convert to numbers
+    sitesIDs<-rownames(axis_data)
+    colnames(axis_data) <- years
+    axis_data<-apply(axis_data, 2, as.numeric)
+    rownames(axis_data)<-sitesIDs
+    
+    # change NAs to zero - can change later if needed
+    axis_data[which(is.na(axis_data))] <- 0
+  
+    ### synchrony - leaving one out
+    ## loop over years
+    for (y in 1: years) {
+    
+    year_removed <- paste(years[y])
+    axis_data_reduced <- axis_data[,-y] 
+    correlation<-cor(t(axis_data_reduced), use = "pairwise.complete.obs")
+    vector_data_correl<- unmatrix(correlation,byrow=F)
+    lower_triangle<-lower.tri(correlation)
+    vector_data_triangle<-unmatrix(lower_triangle, byrow=F)
+    correl_result<-vector_data_correl[vector_data_triangle]
+    site_ID1<-sapply(strsplit(names(correl_result),":"),'[',1)
+    site_ID2<-sapply(strsplit(names(correl_result),":"),'[',2)
+    correl_result
+    synchrony_axis<-rbind(synchrony_axis, cbind(basinsID[basin], 
+                                                as.character(unique(basindata_melt$axis)[ax]),
+                                                correl_result,site_ID1,site_ID2, year_removed))
+  
+    }
+    
+  }
+  
+  
+  
+}
+# warnings()
+# synchrony_axis
+dim(synchrony_axis) #19984257        6
+synchrony_axis<-data.frame(synchrony_axis)
+colnames(synchrony_axis)<-c("basin_ID", "Axis", "Correlation","sYNGEO_ID1","sYNGEO_ID2", "year_removed")
+nlevels(factor(synchrony_axis$basin_ID)) # 46
+nlevels(factor(synchrony_axis$Axis)) # 2
+nlevels(factor(synchrony_axis$year_removed))
+tail(synchrony_axis)
+head(synchrony_axis)
+
+###save results
+# save(synchrony_axis, file = "output_data/results_between_site_synchrony_1-28.RData") ## 1-28 basins
+# load(file = "output_data/results_between_site_synchrony_1-28.RData")
+
+
+save(synchrony_axis, file = "output_data/results_between_site_synchrony_29-46.RData") ## 29-46 basins
+
+# save(synchrony_axis, file = paste0("output_data/results_between_site_synchrony", basinsID[basin],  ".RData"))
